512bd465878c042f7caa266ebe84a0f7d55c49cb
Nick <jiangyoucai1996@126.com>
2020-01-05 14:11:57 +0800

    非阻塞访问,测试通过

---
+++ 10._nonblock/.nonblockio.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/nick/linux/linux_drivers/10._nonblock/nonblockio.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/nick/linux/linux_drivers/10._nonblock/nonblockio.ko /home/nick/linux/linux_drivers/10._nonblock/nonblockio.o /home/nick/linux/linux_drivers/10._nonblock/nonblockio.mod.o
---
+++ 10._nonblock/.tmp_versions/nonblockio.mod
@@ -0,0 +1,2 @@
+/home/nick/linux/linux_drivers/10._nonblock/nonblockio.ko
+/home/nick/linux/linux_drivers/10._nonblock/nonblockio.o
